59fb83bef1d5c3581ffc3a7202b50add83f3ca17
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-02-11 18:22:13 +0530

    modified changes in response according to dotnet api

--- routes/State.routes.js
+++ routes/State.routes.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 import { StateController } from "../controllers/index.js";
 
-router.post("/AddUpdateState", StateController.AddUpdateState);
-router.post("/GetState", StateController.GetState);
+router.post("/AddUpdateStateMaster", StateController.AddUpdateState);
+router.post("/GetStateMaster", StateController.GetState);
 router.delete("/DeleteState", StateController.DeleteState);
 
 export default router;
