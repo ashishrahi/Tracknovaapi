1b7acb6fbdd2d90fe450b5feeafacb4688d06ae7
Asher9889 <saurabhkushwaha9889@gmail.com>
2025-02-12 12:21:49 +0530

    dotnet like response and item type master complete

--- utils/dotnet-like-format/dotnetLikeData.js
+++ utils/dotnet-like-format/dotnetLikeData.js
@@ -1,12 +1,13 @@
 function formattedData(data) {
-    let newObj = {};
-  data.map((obj) => {
+    const response = data.map((obj) => {
+      let newObj = {};
     Object.keys(obj).forEach((key) => {
       let newKey = key.charAt(0).toLowerCase() + key.slice(1);
       newObj[newKey] = obj[key];
     });
+    return newObj;
   });
-  return newObj;
+  return response;
 }
 
 export default formattedData;
