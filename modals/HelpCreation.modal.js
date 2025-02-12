92c911780dcf2b0775bed9102aa4482efc428f74
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-02-12 18:15:46 +0530

    modified changes in  jurisdiction with newnode  api according to dotnet response

--- modals/HelpCreation.modal.js
+++ modals/HelpCreation.modal.js
@@ -3,32 +3,26 @@ import mongoose from 'mongoose';
 const HelpCreateSchema = new mongoose.Schema({
   formatName: {
     type: String,
-    required: true,
     maxlength: 300,
   },
   height: {
     type: Number,
-    required: true,
   },
   width: {
     type: Number,
-    required: true,
   },
   roundedCorner: {
     type: String,
     // enum: ['Y', 'N'],
     default: 'N',
-    required: true,
   },
   variableBackSide: {
     type: String,
     // enum: ['Y', 'N'],
     default: 'N',
-    required: true,
   },
   frontDesign: {
     type: String,
-    required: true,
   },
   backDesign: {
     type: String,
@@ -36,7 +30,6 @@ const HelpCreateSchema = new mongoose.Schema({
   entryDate: {
     type: Date,
     default: Date.now,
-    required: true,
   },
   Page_Name: {
     type: String,
