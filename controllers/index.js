22d59f77b492fae323bfefe929a1243dfbe73aca
Asher9889 <saurabhkushwaha9889@gmail.com>
2025-02-13 15:19:00 +0530

    GetTopFuelConsNTS api completed

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 
-import { probWireTamp , getVehicleNotMoved, sample, SmpCurr, Geofence, NTCurrent, VehCurrStat, GetDashData,GetNTDashboard, GetTopFuelCons, GetTopFuelConsNT, GetTopFuelConsNTOnOff, GetRunningStatus, GetLongIdleVeh, GetVehicleMovement } from "./NTRead.controller.js";
+import { probWireTamp , getVehicleNotMoved, sample, SmpCurr, Geofence, NTCurrent, VehCurrStat, GetDashData,GetNTDashboard, GetTopFuelCons, GetTopFuelConsNT, GetTopFuelConsNTS, GetTopFuelConsNTOnOff, GetRunningStatus, GetLongIdleVeh, GetVehicleMovement } from "./NTRead.controller.js";
 import { getDashboard, getVehicleCurrentDay, getVehicleDistance, getAllBins, getMapBinsWardWise, getvVehicleNo } from "./Dashboard.controller.js";
 import { GetCommGroup, UpsertCommGroup, DeleteCommGroup, GetCommGroupByEmpId, GetAllEmailSetting, UpsertEmailSetting, GetAllSmsSetting, GetCampaignDetailById, GetCampaign, UpsertCampaign, DeleteCampaign, GetCampaignTemplate, UpsertCampaignTemplate, DeleteCampaignTemplate, GetEventSetting, UpsertEventSetting, DeleteEventSetting, GetMasters, UpsertSmsSetting } from "./Comm.controller.js";
 import { AddUpdateItemCategory, GetItemCategory, DeleteItemCategory } from "./ItemCategory.controller.js"
@@ -27,6 +27,7 @@ export const NTReadController = {
     GetNTDashboard: GetNTDashboard,
     GetTopFuelCons: GetTopFuelCons,
     GetTopFuelConsNT: GetTopFuelConsNT,
+    GetTopFuelConsNTS: GetTopFuelConsNTS,
     GetTopFuelConsNTOnOff: GetTopFuelConsNTOnOff,
     GetRunningStatus: GetRunningStatus,
     GetLongIdleVeh: GetLongIdleVeh,
